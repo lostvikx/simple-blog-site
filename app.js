7f808d1956ffe5198425bfc5b24b4a343c7be020
Vikram Negi <viknegi0@gmail.com>
2021-08-15 13:41:20 +0530

    rm sandbox mongodb

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
 const uri = "mongodb+srv://vik:HzoU1KXELSUELF6p@simple-blog-tut.l76kf.mongodb.net/simple-blog-tut?retryWrites=true&w=majority";
 
 // connect to db
+
 // imp note: we want our server to listen on port 3000, only after the db is loaded
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(3000 || process.env.PORT, () => console.log("live on port 3000")))
@@ -21,6 +22,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // register view engine
 app.set("view engine", "ejs");
+
 // views dir is default
 // app.set("views", "dirName");
 
@@ -29,63 +31,17 @@ app.set("view engine", "ejs");
 // static files {imgs, css...}
 app.use(express.static("public"));
 
-// middleware
+// form data from create.ejs
 app.use(express.urlencoded({ extended: true }));
-
-
-// custom logger middleware
-// app.use((req, res, next) => {
-//   console.log("new request was made");
-//   console.log("host:", req.hostname);
-//   console.log("path:", req.path);
-//   console.log("method:", req.method);
-//   // allows us to move onto the next middleware function
-//   next();
-// });
 
 // logger middleware {npm}
 app.use(morgan("tiny"));
-
-// // mongoose and mongo sandbox routes
-// // save documents in db
-// app.get("/add-blog", (req, res)=> {
-
-//   const blog = new Blog({
-//     title: "New Blog 2",
-//     body: "This is the blog body"
-//   });
-
-//   // async task
-//   blog.save()
-//     .then(result => res.send(result))
-//     .then(err => console.log(err));
-// });
-
-// // get all documents
-// app.get("/all-blogs", (req, res) => {
-//   // async task
-//   Blog.find()
-//     .then(result => res.send(result))
-//     .catch(err => console.log(err));
-// });
-
-// // get a single blog
-// app.get("/single-blog", (req, res) => {
-//   // async task
-//   // id of first blog
-//   Blog.findById("6113ff3f48cb2d3da2ca34d1")
-//     .then(result => res.send(result))
-//     .catch(err => console.log(err));
-// });
-
-// rendering a view
 
 // routes
 
 app.get("/", (req, res) => {
   // redirct to /blogs
   res.redirect("/blogs");
-  // res.render("index", { "title": "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
@@ -101,7 +57,6 @@ app.get("/about-me", (req, res) => {
 app.use("/blogs", blogRoutes);
 
 // 404 page
-
 app.use( (req, res) => {
   res.status(404).render("404", { "title": "404 Error" });
   app.set("Content-Type", "text/html");
